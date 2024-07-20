bc75d1ad63fff56fb71aa2d762f2641764dfe993
Ansh Goyal <ansh172003@gmail.com>
2024-07-20 21:39:39 +0530

    model and images fixes

--- src/addtocart.js
+++ src/addtocart.js
@@ -17,4 +17,16 @@ export function addToCart(index, quantity) {
     }
     console.log(cart)
     localStorage.setItem('cart', JSON.stringify(cart));
+    showSnackbar();
 };
+
+function showSnackbar() {
+    // Get the snackbar DIV
+    var snackbar = document.getElementById("snackbar");
+
+    // Add the "show" class to DIV
+    snackbar.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 3000);
+}
